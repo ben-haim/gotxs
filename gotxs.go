b20d6e5704c6783adf498f2ddc4ebf10e5a5dcd0
Otto Allmendinger <otto.allmendinger@gmail.com>
2014-09-18 14:51:58 +0200

    Remove custom error type and improve doc

--- gotxs.go
+++ gotxs.go
@@ -1,28 +1,22 @@
+// An ideomatic go wrapper for the SWIG-generated go interface
+//
+// This package sets up all the required objects, translates error conditions to
+// the go way of doing things, and provides some documentation.
 package gotxs
 
 import "errors"
 
+// this are the lower-level, SWIG-generated bindings
 import "gotxs/opentxs"
 
+// Some API methods go through an instance of this object.
 var otme opentxs.OT_ME
 
-type apiCallError struct {
-	method  string
-	message string
-}
-
-func (e *apiCallError) Error() string {
-	return e.message
-}
-
+// Create a new pseudonym in the local wallet.
+// Crashes with OT_FAIL if keysize is invalid.
+// Returns generated pseudonym id.
 func CreatePseudonym(
 	keybits int, nym_id_source, alt_location string) (string, error) {
-
-	/*
-		Create a new pseudonym in the local wallet.
-		Crashes with OT_FAIL if keysize is invalid.
-		Returns generated pseudonym id.
-	*/
 
 	retval := otme.Create_pseudonym(keybits, nym_id_source, alt_location)
 
@@ -33,6 +27,9 @@ func CreatePseudonym(
 	}
 }
 
+// The initialization can be done automatically. This function is called when
+// the module is imported first. It calls the init functions of the opentxs api
+// and creates a reference to a `OTME` object.
 func init() {
         if opentxs.OTAPI_WrapAppInit() == false {
                 panic("error in OTAPI_WrapAppInit()")
