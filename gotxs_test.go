e07f8cec29600b729ba4f8586a16d72d69ac1a67
Otto Allmendinger <otto.allmendinger@gmail.com>
2014-09-18 15:43:05 +0200

    restructure basic API test

--- gotxs_test.go
+++ gotxs_test.go
@@ -1,11 +1,26 @@
+// This is just a very basic test to see if the gotxs/opentxs package
+// compiled correctly. 
+//
+// Run 'go test -v'
 package gotxs_test
 
 import "testing"
 
-import . "gotxs"
+import "gotxs"
 
-func TestCreateNym(t *testing.T) {
-    retval, err := CreatePseudonym(1024, "", "")
+// just make a basic api call and Cleanup() after that
+func TestBasicApi(t *testing.T) {
+	keysize := 1024
+	nym_source := ""
+	alt_location := ""
 
-    t.Logf("done", retval, err)
+	retval, err := gotxs.CreatePseudonym(keysize, nym_source, alt_location)
+
+	if err == nil {
+		t.Logf("created new pseudonym %s", retval)
+	} else {
+		t.Error("could not create new pseudoynm")
+	}
+
+	gotxs.Cleanup()
 }
