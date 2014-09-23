ee17c9380b4bba0fb3a586be188cabff475f16d6
Otto Allmendinger <otto.allmendinger@gmail.com>
2014-09-23 22:13:43 +0200

    Fix documentation, move to camelCase notation
    I used snake notation first because I was porting the python3 code.

--- easy/easy.go
+++ easy/easy.go
@@ -9,20 +9,21 @@ import "github.com/monetas/gotxs/opentxs"
 
 var otme opentxs.OT_ME
 
-// Create a new pseudonym in the local wallet.
-// Crashes with OT_FAIL if keysize is invalid.
+// CreatePseudonym creates a new pseudonym in the local wallet.
+// Keybits must be one of 1024, 2048, 4096 or 8192
+// The arguments ny_id_source and alt_location are optional.
 // Returns generated pseudonym id.
-func CreatePseudonym(
-	keybits int, nym_id_source, alt_location string) (string, error) {
+func CreatePseudonym(keybits int, nymIdSource, altLocation string) (string, error) {
 
-	retval := otme.Create_pseudonym(keybits, nym_id_source, alt_location)
+	retval := otme.Create_pseudonym(keybits, nymIdSource, altLocation)
 
 	if retval == "" {
-		return "", errors.New("empty response")
-	} else {
-		return retval, nil
+		return "", errors.New("empty return value")
 	}
+
+	return retval, nil
 }
+
 
 // create a reference to a OT_ME() instance
 // needs gotxs import which calls init()
